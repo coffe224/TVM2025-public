77aa3382bfccb73e995b3e22986c8f887b544577
coffe224 <s.chernykh@g.nsu.ru>
2025-12-08 06:53:08 +0700

    lab5

--- lab05/src/compiler.ts
+++ lab05/src/compiler.ts
@@ -3,9 +3,29 @@ import { Expr } from "@tvm/lab04";
 import { buildOneFunctionModule, Fn } from "./emitHelper";
 const { i32, get_local} = C;
     
+
 export function getVariables(e: Expr): string[] {
-    throw "Not implemented";
+    return [...new Set(findVariables(e))];
 }
+
+export function findVariables(e: Expr): string[] {
+    switch (e.type) {
+        case 'variable':
+            return [e.value];
+
+        case 'number':
+            return [];
+
+        case 'unary_min':
+        case 'brac_expr':
+            return findVariables(e.arg);
+
+        case 'mul_op':
+        case 'add_op':
+            return e.args.flatMap(arg => findVariables(arg));
+    }
+}
+
 
 export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<number>>
 {
@@ -14,5 +34,46 @@ export async function buildFunction(e: Expr, variables: string[]): Promise<Fn<nu
 }
 
 function wasm(e: Expr, args: string[]): Op<I32> {
-    throw "Not implemented";
+    switch (e.type) {
+        case 'number':
+            return i32.const(e.value);
+
+        case 'variable':
+            const index = args.indexOf(e.value);
+            return get_local(i32, index);
+
+        case 'unary_min':
+            return i32.sub(i32.const(0), wasm(e.arg, args));
+
+        case 'brac_expr':
+            return wasm(e.arg, args);
+
+        case 'add_op':
+            let add_result : Op<I32> = wasm(e.args[0], args);
+
+            for (let i = 0; i < e.ops.length; i++) {
+                if (e.ops[i] == "+") {
+                    add_result = i32.add(add_result, wasm(e.args[i + 1], args));
+                } else if (e.ops[i] == "-") {
+                    add_result = i32.sub(add_result, wasm(e.args[i + 1], args));
+                } else {
+                    throw Error;
+                }
+            }
+            return add_result;
+
+        case 'mul_op':
+            let mul_result : Op<I32> = wasm(e.args[0], args);
+
+            for (let i = 0; i < e.ops.length; i++) {
+                if (e.ops[i] == "*") {
+                    mul_result = i32.mul(mul_result, wasm(e.args[i + 1], args));
+                } else if (e.ops[i] == "/") {
+                    mul_result = i32.div_s(mul_result, wasm(e.args[i + 1], args));
+                } else {
+                    throw Error;
+                }
+            }
+            return mul_result;
+    }
 }
